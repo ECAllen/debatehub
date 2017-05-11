37f23bde2de54cbd4e7e3fc8e7f6cb9b7c623eca
Ethan Allen <ethancallen@gmail.com>
2017-05-10 22:49:06 -0400

    updated email

--- actions/email.go
+++ actions/email.go
@@ -4,6 +4,7 @@ import "github.com/gobuffalo/buffalo"
 
 // EmailHandler for submitted emails
 func EmailHandler(c buffalo.Context) error {
+	c.Set("name", c.Get("emailAddr"))
 	return c.Render(200, r.HTML("email.html"))
 	// return c.Render(200, r.String(c.Param("emailAddr")))
 }
