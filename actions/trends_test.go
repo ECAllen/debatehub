644791b9944199019d3c66799d51afe52e27eea3
Ethan Allen <ethancallen@gmail.com>
2017-06-26 23:07:46 -0400

    added trends resource

---
+++ actions/trends_test.go
@@ -0,0 +1,29 @@
+package actions_test
+
+func (as *ActionSuite) Test_TrendsResource_List() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TrendsResource_Show() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TrendsResource_New() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TrendsResource_Create() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TrendsResource_Edit() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TrendsResource_Update() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TrendsResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
---
+++ models/trend.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/markbates/pop"
+	"github.com/markbates/pop/nulls"
+	"github.com/markbates/validate"
+	"github.com/markbates/validate/validators"
+	"github.com/satori/go.uuid"
+)
+
+type Trend struct {
+	ID          uuid.UUID    `json:"id" db:"id"`
+	CreatedAt   time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at" db:"updated_at"`
+	Summary     string       `json:"summary" db:"summary"`
+	Description string       `json:"description" db:"description"`
+	Thumbnail   nulls.String `json:"thumbnail" db:"thumbnail"`
+	Publlished  nulls.Bool   `json:"publlished" db:"publlished"`
+	Rejected    nulls.Bool   `json:"rejected" db:"rejected"`
+}
+
+// String is not required by pop and may be deleted
+func (t Trend) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Trends is not required by pop and may be deleted
+type Trends []Trend
+
+// String is not required by pop and may be deleted
+func (t Trends) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Validate gets run everytime you call a "pop.Validate" method.
+// This method is not required and may be deleted.
+func (t *Trend) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: t.Summary, Name: "Summary"},
+		&validators.StringIsPresent{Field: t.Description, Name: "Description"},
+	), nil
+}
+
+// ValidateSave gets run everytime you call "pop.ValidateSave" method.
+// This method is not required and may be deleted.
+func (t *Trend) ValidateSave(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run everytime you call "pop.ValidateUpdate" method.
+// This method is not required and may be deleted.
+func (t *Trend) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
